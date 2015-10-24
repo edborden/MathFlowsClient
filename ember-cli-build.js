786efdf9ea063576647c0ad0d107bf293c0d12b3
edborden <borden.edward@gmail.com>
2015-10-24 12:51:45 -0400

    favicon

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -16,6 +16,7 @@ module.exports = function(defaults) {
 	app.options.inlineContent['droidFont'] = {content:"<link href='https://fonts.googleapis.com/css?family=Damion|Droid+Sans' rel='stylesheet' type='text/css'>"};
 	app.options.inlineContent['desmos'] = {content:"<script src='https://www.desmos.com/api/v0.4/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6'></script>"};
 	app.options.inlineContent['cloudinary'] = {content:"<script src='//widget.cloudinary.com/global/all.js' type='text/javascript'></script>"};
+	app.options.inlineContent['favicon'] = {content:"<link rel='icon' href='/assets/images/hourglass.ico'>"};
 
 	if (config.environment === 'production') {
 		app.options.inlineContent['inspectlet'] = {file:"inspectlet.js"};
